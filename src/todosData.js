5ff26e8f4c4fca1a1202004b3ad64f6f62e55ec0
snigdha2210 <f20190555@goa.bits-pilani.ac.in>
2020-09-13 16:26:06 +0530

    todo static app with react

---
+++ src/todosData.js
@@ -0,0 +1,39 @@
+const todosData = [
+  {
+    id: 1,
+    text: "Learn Typescript",
+    completed: false
+  },
+  {
+    id: 2,
+    text: "Complete Lecture series on youtube",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "Clean gecko tank",
+    completed: false
+  },
+  {
+    id: 4,
+    text: "Mow lawn",
+    completed: true
+  },
+  {
+    id: 5,
+    text: "Catch up on Arrested Development",
+    completed: false
+  },
+  {
+    id: 6,
+    text: "Take out the trash",
+    completed: false
+  },
+  {
+    id: 7,
+    text: "Grocery shopping",
+    completed: true
+  }
+];
+
+export default todosData;
